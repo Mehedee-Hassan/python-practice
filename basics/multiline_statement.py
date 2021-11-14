a3d3019ea7c3e22106c812337caefb4988cc880d
Mehedee Hassan <Mehedee-Hassan@users.noreply.github.com>
2021-11-14 21:31:54 +0900

    moved multiline_statement

---
+++ basics/multiline_statement.py
@@ -0,0 +1,22 @@
+# multiline statements
+
+
+a = {1,
+3,
+4} # this is a set
+print(a)
+a.add(10)
+
+print(a)
+
+
+aTuple = (1,2,3
+	,4)
+
+print(aTuple)
+
+# multi line
+b,c,d = 1,2\
+,3
+
+y = 1;g = 2
